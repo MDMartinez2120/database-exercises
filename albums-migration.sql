8cb8dc41ebb681933941ecf764277b4b86c0672b
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-18 16:41:01 -0500

    finished part 1 of basic statements.

--- albums-migration.sql
+++ albums-migration.sql
@@ -12,6 +12,29 @@ CREATE TABLE IF NOT EXISTS albums (
     PRIMARY KEY (id)
 );
 
-DESCRIBE albums;
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES('kanye west', 'late registration', 2006, 12345678, 'hip-hop');
 
-SHOW CREATE TABLE albums;
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES('foo fighters', 'in your honor', 2007, 12345678, 'rock');
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES('minus the bear', 'planet of ice', 2006, 12345678, 'progressive');
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES('kendrick lamar', 'alright', 2018, 12345678, 'hip-hop');
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES('j-cole', 'KOD', 2020, 12345678, 'hip-hop');
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('kendrick lamar', 'alright', 2018, 12345678, 'hip-hop'),
+       ('j-cole', 'KOD', 2020, 12345678, 'hip-hop'),
+       ('minus the bear', 'planet of ice', 2006, 12345678, 'progressive'),
+       ('foo fighters', 'in your honor', 2007, 12345678, 'rock'),
+       ('kanye west', 'late registration', 2006, 12345678, 'hip-hop');
+
+
+# DESCRIBE albums;
+#
+# SHOW CREATE TABLE albums;
