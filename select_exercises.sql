d9754f8ee66be5d5b08aa286e7ffa2af8d187808
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-18 20:15:14 -0500

    finished part 3 of the basic statements exercises.

---
+++ select_exercises.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+SELECT name as 'the name of all albums from pink floyd'
+FROM albums WHERE artist = 'pink floyd';
+
+SELECT release_date as 'sgt. peppers was released'
+FROM albums WHERE name = 'sgt. peppers lonely hearts club band';
+
+SELECT genre as 'the genre for nevermind'
+FROM albums WHERE name = 'nevermind';
+
+SELECT name as 'the albums that were released in the 1990s'
+FROM albums WHERE release_date = YEAR(release_date) BETWEEN 1990 AND 1999;
+
+SELECT name as 'albums had less than 20 million certified sales' FROM albums WHERE sales < 20000000;
+
+SELECT name as 'the albums with a genre of rock' FROM albums WHERE genre = 'rock';
