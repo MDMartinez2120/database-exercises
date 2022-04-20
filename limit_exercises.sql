4b82f7f2e06b5f7d779917d75d825f306d5de983
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-20 09:27:22 -0500

    fixed the first 2 solutions of my limit exercise.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 
-SELECT DISTINCT title FROM titles;
+SELECT DISTINCT title FROM titles LIMIT 10;
 
-SELECT DISTINCT last_name FROM employees;
+SELECT DISTINCT last_name FROM employees LIMIT 5;
 
 SELECT DISTINCT emp_no FROM salaries LIMIT 10;
